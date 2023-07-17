2047b9843fa376b005798cd4c88a9888828074c5
Spandan <spandandec21@gmail.com>
2023-07-17 16:31:26 +0545

    Incorrect Password Message

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -12,6 +12,7 @@ const Login = () => {
 
     const searchParams = useSearchParams()
     const success = searchParams.get('success')
+    const error = searchParams.get('error')
 
     // if (session.status === 'loading') {
     //     return <p>Loading...</p>
@@ -37,6 +38,7 @@ const Login = () => {
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <span className='py-2 text-green-400'>{success || ""}</span>
+                        <span className='py-2 text-red-400'>{error || ""}</span>
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Sign in to your account
                         </h1>
